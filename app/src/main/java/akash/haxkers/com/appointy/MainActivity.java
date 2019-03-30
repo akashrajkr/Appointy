50b6abad17b28e63ace09f6dd0115e3fc7d72c2b
Akash Raj <aakashrajkr@gmail.com>
2019-03-31 00:30:25 +0530

    commit

--- app/src/main/java/akash/haxkers/com/appointy/MainActivity.java
+++ app/src/main/java/akash/haxkers/com/appointy/MainActivity.java
@@ -2,12 +2,18 @@ package akash.haxkers.com.appointy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
